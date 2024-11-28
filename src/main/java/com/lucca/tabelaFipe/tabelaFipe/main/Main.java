1a2fdca36b43d20c84f26e98edef54c9b0c459c0
lucas <lucaston2323@gmail.com>
2024-11-28 15:51:35 -0300

    feat: estruturando menu de interação

--- src/main/java/com/lucca/tabelaFipe/tabelaFipe/main/Main.java
+++ src/main/java/com/lucca/tabelaFipe/tabelaFipe/main/Main.java
@@ -1,8 +1,45 @@
 package com.lucca.tabelaFipe.tabelaFipe.main;
 
-public class Main {
+import com.lucca.tabelaFipe.tabelaFipe.service.TabelaFipeClient;
 
-    public void menu() {
-        System.out.println("Hello");
+import java.util.Scanner;
+
+public class Main {
+    private Scanner sc = new Scanner(System.in);
+
+    public void exibeMenu() {
+        TabelaFipeClient client = new TabelaFipeClient();
+
+        var menu = """
+                ============ OPÇÕES ============
+                1. Carro
+                2. Moto
+                3. Caminhão
+                
+                Digite a opção desejada:
+                """;
+
+        System.out.println(menu);
+
+        var opcao = sc.nextLine();
+        String endereco;
+
+        switch (opcao) {
+            case "1":
+                endereco = "carro";
+                break;
+            case "2":
+                endereco = "moto";
+                break;
+            case "3":
+                endereco = "caminhao";
+                break;
+            default:
+                System.out.println("Opção inválida, tente novamente!");
+                return;
+        }
+
+        var json = client.obterDados(endereco);
+        System.out.println(json);
     }
 }
