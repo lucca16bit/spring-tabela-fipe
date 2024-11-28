de9109cb9928379d768006e9933aba09420a292e
lucas <lucaston2323@gmail.com>
2024-11-28 15:50:38 -0300

    feat: adicionando endpoint

--- src/main/java/com/lucca/tabelaFipe/tabelaFipe/service/TabelaFipeClient.java
+++ src/main/java/com/lucca/tabelaFipe/tabelaFipe/service/TabelaFipeClient.java
@@ -1,35 +1,34 @@
 package com.lucca.tabelaFipe.tabelaFipe.service;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.Properties;
 
 public class TabelaFipeClient {
-    private static final String API_KEY;
     private static final String URL_BASE = "https://parallelum.com.br/fipe/api/v1";
 
-    static {
-        Properties properties = new Properties();
-        try (InputStream input = new FileInputStream("src/resources/config.properties")) {
-            if (input == null) {
-                throw new IOException("Configuração de arquivo não encontrada!");
-            }
-            properties.load(input);
-            API_KEY = properties.getProperty("FIPE_API_KEY");
-        } catch (IOException e) {
-            throw new RuntimeException("Falha ao carregar a chave da API.", e);
-        }
-    }
-
     public String obterDados(String endereco) {
+        String endpoint;
+
+        switch (endereco.toLowerCase()) {
+            case "carro":
+                endpoint = URL_BASE + "/carros/marcas";
+                break;
+            case "moto":
+                endpoint = URL_BASE + "/motos/marcas";
+                break;
+            case "caminhao":
+                endpoint = URL_BASE + "/caminhoes/marcas";
+                break;
+            default:
+                throw new IllegalArgumentException("Entrada inválida, tente novamente!");
+        }
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(endereco))
+                .uri(URI.create(endpoint))
                 .build();
         HttpResponse<String> response = null;
         try {
