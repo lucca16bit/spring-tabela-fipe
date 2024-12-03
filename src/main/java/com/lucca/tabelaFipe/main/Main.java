427839a427a2fc18942acfdd3c03db079a2ef313
lucas <lucaston2323@gmail.com>
2024-12-03 16:03:58 -0300

    feat: fazendo buscas e filtrando buscas

--- src/main/java/com/lucca/tabelaFipe/main/Main.java
+++ src/main/java/com/lucca/tabelaFipe/main/Main.java
@@ -1,12 +1,16 @@
 package com.lucca.tabelaFipe.main;
 
+import com.lucca.tabelaFipe.model.Veiculo;
 import com.lucca.tabelaFipe.service.ConverteDados;
 import com.lucca.tabelaFipe.model.Dados;
 import com.lucca.tabelaFipe.model.Modelos;
 import com.lucca.tabelaFipe.service.TabelaFipeClient;
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
     private final String URL_BASE = "https://parallelum.com.br/fipe/api/v1/";
@@ -33,7 +37,6 @@ public class Main {
         };
 
         var json = client.obterDados(endereco);
-        System.out.println(json);
 
         var marcas = converteDados.obterLista(json, Dados.class);
         marcas.stream()
@@ -42,6 +45,7 @@ public class Main {
 
         System.out.println("Informe o código da marca para consulta: ");
         int codigoMarca = sc.nextInt();
+        sc.nextLine();
 
         endereco = endereco + "/" + codigoMarca + "/modelos";
         json = client.obterDados(endereco);
@@ -51,5 +55,33 @@ public class Main {
         modeloLista.modelos().stream()
                 .sorted(Comparator.comparing(Dados::codigo))
                 .forEach(System.out::println);
+
+        System.out.println("\nDigite um trecho do nome do carro a ser buscado:");
+        String nomeVeiculo = sc.nextLine();
+
+        List<Dados> modelosFiltrados = modeloLista.modelos().stream()
+                .filter(m -> m.nome().toLowerCase().contains(nomeVeiculo.toLowerCase()))
+                .collect(Collectors.toList());
+
+        System.out.println("\nModelos filtrados: ");
+        modelosFiltrados.forEach(System.out::println);
+
+        System.out.println("Digite por favor o código do modelo para buscar os valores de avaliação:");
+        var codigoModelo = sc.nextLine();
+
+        endereco += "/" + codigoModelo + "/anos";
+        json = client.obterDados(endereco);
+        List<Dados> anos = converteDados.obterLista(json, Dados.class);
+        List<Veiculo> veiculos = new ArrayList<>();
+
+        for (int i = 0; i < anos.size(); i++) {
+            var enderecoAnos = endereco + "/" + anos.get(i).codigo();
+            json = client.obterDados(enderecoAnos);
+            Veiculo veiculo = converteDados.obterDados(json, Veiculo.class);
+            veiculos.add(veiculo);
+        }
+
+        System.out.println("\nTodos os veiculos filtrados com avaliaçoes por ano:");
+        veiculos.forEach(System.out::println);
     }
 }
